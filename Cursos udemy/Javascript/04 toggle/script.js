503927793c7aca9a7e515266fceffbcb2f5627b8
AlexHz01 <yagamilonzoro@gmail.com>
2023-01-30 17:49:20 -0600

    actulizar

--- Cursos udemy/Javascript/04 toggle/script.js
+++ Cursos udemy/Javascript/04 toggle/script.js
@@ -1,7 +1,5 @@
-let toggleBtn = document.querySelector('.toggleBtn');
-
-let container = document.querySelector('.container');
-
-toggleBtn.onclick = function() {
-    container.classList.toggle('active');
+let toggleBtn =document.querySelector('.toggleBtn')
+let container=document.querySelector('.container')
+toggleBtn.onclick=function(){
+    container.classList.toggle('active')
 }
