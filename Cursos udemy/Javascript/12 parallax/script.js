d89ae17efc33889e4b5bcedc4d8605f199aa9cc5
AlexHz01 <yagamilonzoro@gmail.com>
2023-02-17 17:52:17 -0600

    componer_parallax

---
+++ Cursos udemy/Javascript/12 parallax/script.js
@@ -0,0 +1,5 @@
+
+let scene=document.getElementById('imagenes');
+let text = document.getElementById('text');
+let parallaxInstanceScene= new Parallax(imagenes);
+let parallaxInstanceText= new Parallax(text);
