3a82e24e39ae0bd04efeb43870c188f7a5ca60a1
AlexHz01 <yagamilonzoro@gmail.com>
2023-02-25 14:00:12 -0600

    s

--- Cursos udemy/Javascript/15 pokedex/script.js
+++ Cursos udemy/Javascript/15 pokedex/script.js
@@ -16,12 +16,54 @@ document.addEventListener("DOMContentLoaded", function() {
         return data.pokemon_species;
     };
 
-    async function getPokemons(numero){
+    function orderNumber(str){
+        var mySubstring = str.substring(
+            str.lastIndexOf("s/") + 2 , str.lastIndexOf("/")
+        );
+        return mySubstring;
+    }
+
+    async function getPokemons(numero, toggle){
         let endpoint = `https://pokeapi.co/api/v2/generation/${numero}/`;
         var container = document.getElementById("container");
         container.innerHTML = "";
-        let pokemons = await fetchPokemons(endpoint);
-        console.log(pokemons);
+        let pokemons = [] 
+        pokemons = await fetchPokemons(endpoint);
+        for (let j = 0; j < pokemons.length; j++) {
+
+            pokemons[j].nr = orderNumber(pokemons[j].url);
+        }
+        pokemons.sort((a,b)=>a.nr-b.nr);
+        
+        pokemons.forEach((item)=>{
+            let numero3decimales = orderNumber(item.nr);
+            if(numero3decimales<10){
+                numero3decimales = "0"+numero3decimales;
+            }
+            if (numero3decimales<100){
+                numero3decimales = "0"+numero3decimales;
+            }
+
+            let divItem = document.createElement("li");
+            divItem.classList.add("item");
+            var img = new Image();
+            const toggleurl = toggle ? "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"
+            : "https://www.serebii.net/pokemongo/pokemon/";
+            img.src = "https://i.gifer.com/origin/28/2860d2d8c3a1e402e0fc8913cd92cd7a_w200.gif";
+            
+            const urlImage = `${toggleurl}${numero3decimales}.png`;
+            img.setAttribute("data-image", urlImage);
+            img.setAttribute("class", "pokeimage");
+            img.setAttribute("alt", item.name);
+
+
+            divItem.innerHTML= `<div> ${orderNumber(item.url)}-${item.name}</div>`
+            divItem.appendChild(img);
+            container.appendChild(divItem);
+        })
+
+        console.log(pokemons)
+ 
     }
     getPokemons(2);
 
